6b659063074b53e97c27ac8e652b186a1f598a33
cqconelin <18910587228@189.cn>
2018-10-17 18:19:59 +0800

    javascript 脚本。

--- nnlightctl/js/api/requestRoot.js
+++ nnlightctl/js/api/requestRoot.js
@@ -1,2 +1,13 @@
-const BASE_ROOT = "http://103.48.232.122:8080/nnlightctl";
+const BASE_ROOT = "http://103.48.232.121:8080/nnlightctl";
 const PAGE_SIZE = 10;
+
+const SEX = {
+    0 : "女",
+    1 : "男"
+};
+
+const USER_STATE = {
+    0 : "删除",
+    1 : "正常",
+    2 : "锁定"
+};
