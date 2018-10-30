df6964e6645c735803426cdf06761bc96e7bc753
cqconelin <18910587228@189.cn>
2018-10-30 13:32:16 +0800

    javascript 脚本。

--- nnlightctl/js/admin.js
+++ nnlightctl/js/admin.js
@@ -156,48 +156,12 @@ function removeIframeAll(){
 }
 
 /*弹出层*/
-/*
-	参数解释：
-	title	标题
-	url		请求的url
-	id		需要操作的数据id
-	w		弹出层宽度（缺省调默认值）
-	h		弹出层高度（缺省调默认值）
-*/
-function layer_show(title,url,w,h){
-	if (title == null || title == '') {
-		title=false;
-	};
-	if (url == null || url == '') {
-		url="404.html";
-	};
-	if (w == null || w == '') {
-		w=800;
-	};
-	if (h == null || h == '') {
-		h=($(window).height() - 50);
-	};
-	layer.open({
-		type: 2,
-		area: [w+'px', h +'px'],
-		fix: false, //不固定
-		maxmin: true,
-		shade:0.4,
-		title: title,
-		content: url
-	});
-}
+function layer_show() {
+	var title = arguments[0];
+	var url = arguments[1];
+	var w = arguments[2];
+	var h = arguments[3];
 
-/*
-	参数解释：
-	title	标题
-	url		请求的url
-	id		需要操作的数据id
-	w		弹出层宽度（缺省调默认值）
-	h		弹出层高度（缺省调默认值）
-	callbackfun 回调函数
-*/
-function layer_show(title,url,w,h,callbackfun){
     if (title == null || title == '') {
         title=false;
     };
@@ -210,29 +174,124 @@ function layer_show(title,url,w,h,callbackfun){
     if (h == null || h == '') {
         h=($(window).height() - 50);
     };
-    layer.open({
-        type: 2,
-        area: [w+'px', h +'px'],
-        fix: false, //不固定
-        maxmin: true,
-        shade:0.4,
-        title: title,
-        content: url,
-        btn: ['确定','关闭'],
-        yes: function(index){
-            //当点击‘确定’按钮的时候，获取弹出层返回的值
-            var res = window["layui-layer-iframe" + index].getData();
-            //打印返回的值，看是否有我们想返回的值。
-            console.log(res);
-            callbackfun(res);
-            //最后关闭弹出层
-            layer.close(index);
-        },
-        cancel: function(){
-            //右上角关闭回调
-        }
-    });
+
+	switch (arguments.length) {
+		case 4:
+            layer.open({
+                type: 2,
+                area: [w+'px', h +'px'],
+                fix: false, //不固定
+                maxmin: true,
+                shade:0.4,
+                title: title,
+                content: url
+            });
+			break;
+
+		case 5:
+			var callbackfun = arguments[4];
+            layer.open({
+                type: 2,
+                area: [w+'px', h +'px'],
+                fix: false, //不固定
+                maxmin: true,
+                shade:0.4,
+                title: title,
+                content: url,
+                btn: ['确定','关闭'],
+                yes: function(index){
+                    //当点击‘确定’按钮的时候，获取弹出层返回的值
+                    var res = window["layui-layer-iframe" + index].getData();
+                    //打印返回的值，看是否有我们想返回的值。
+                    console.log(res);
+                    callbackfun(res);
+                    //最后关闭弹出层
+                    layer.close(index);
+                },
+                cancel: function(){
+                    //右上角关闭回调
+                }
+            });
+			break;
+	}
 }
+/*
+	参数解释：
+	title	标题
+	url		请求的url
+	id		需要操作的数据id
+	w		弹出层宽度（缺省调默认值）
+	h		弹出层高度（缺省调默认值）
+*/
+// function layer_show(title,url,w,h){
+// 	if (title == null || title == '') {
+// 		title=false;
+// 	};
+// 	if (url == null || url == '') {
+// 		url="404.html";
+// 	};
+// 	if (w == null || w == '') {
+// 		w=800;
+// 	};
+// 	if (h == null || h == '') {
+// 		h=($(window).height() - 50);
+// 	};
+// 	layer.open({
+// 		type: 2,
+// 		area: [w+'px', h +'px'],
+// 		fix: false, //不固定
+// 		maxmin: true,
+// 		shade:0.4,
+// 		title: title,
+// 		content: url
+// 	});
+// }
+
+/*
+	参数解释：
+	title	标题
+	url		请求的url
+	id		需要操作的数据id
+	w		弹出层宽度（缺省调默认值）
+	h		弹出层高度（缺省调默认值）
+	callbackfun 回调函数
+*/
+// function layer_show(title,url,w,h,callbackfun){
+//     if (title == null || title == '') {
+//         title=false;
+//     };
+//     if (url == null || url == '') {
+//         url="404.html";
+//     };
+//     if (w == null || w == '') {
+//         w=800;
+//     };
+//     if (h == null || h == '') {
+//         h=($(window).height() - 50);
+//     };
+//     layer.open({
+//         type: 2,
+//         area: [w+'px', h +'px'],
+//         fix: false, //不固定
+//         maxmin: true,
+//         shade:0.4,
+//         title: title,
+//         content: url,
+//         btn: ['确定','关闭'],
+//         yes: function(index){
+//             //当点击‘确定’按钮的时候，获取弹出层返回的值
+//             var res = window["layui-layer-iframe" + index].getData();
+//             //打印返回的值，看是否有我们想返回的值。
+//             console.log(res);
+//             callbackfun(res);
+//             //最后关闭弹出层
+//             layer.close(index);
+//         },
+//         cancel: function(){
+//             //右上角关闭回调
+//         }
+//     });
+// }
 
 /*关闭弹出框口*/
 function layer_close(){
